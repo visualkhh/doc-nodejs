26a758805f1efe3e03d9f8de82818c52b4a9626f
khh <visualkhh@gmail.com>
2021-06-05 09:24:21 +0900

    g

--- npm/workspaces/test1/project/src/app/app.module.ts
+++ npm/workspaces/test1/project/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 // import { AppComponent } from './app.component';
-import { AppComponent } from 'cores/app.component';
+// import { AppComponent } from 'cores/app.component';
+import { AppComponent } from 'cores/app.component'
 
 @NgModule({
   declarations: [
