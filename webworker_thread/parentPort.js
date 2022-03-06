538e74a126684ce29c8fc4ccf7ccdf4ed482b82a
visualkhh <visualkhh@gmail.com>
2022-03-06 10:35:11 +0900

    workthread

--- webworker_thread/parentPort.js
+++ webworker_thread/parentPort.js
@@ -1,4 +1,4 @@
-const { Worker, isMainThread, parentPort } = require('worker_threads');
+const { Worker, isMainThread, parentPort, threadId } = require('worker_threads');
 class User {
     constructor(name, age) {
         this.name = name;
@@ -14,14 +14,26 @@ if (isMainThread) {
         console.log(message);  // Prints 'Hello, world!'.
     });
     // worker.postMessage('Hello, world!');
-    worker.postMessage({name: 'hhh', say: new User(), sayType: User});
+    worker.postMessage({name: 'hhh'});
+    worker.postMessage({name: 'hh2h'});
+    worker.postMessage({name: 'hh2h'});
+
+
+    const worker2 = new Worker(__filename);
+    worker2.once('message', (message) => {
+        console.log(message);  // Prints 'Hello, world!'.
+    });
+    worker2.postMessage({name: '2hhh'});
+    worker2.postMessage({name: '2hh2h'});
+    worker2.postMessage({name: '2hh2h'});
     // worker.postMessage({name: 'Hello, world!', say: () => {
     //         console.log('Hello, world!');
     //     }});
 } else {
     // When a message from the parent thread is received, send it back:
-    parentPort.once('message', (message) => {
-        console.log('---------', message);
+    // parentPort.once('message', (message) => {
+    parentPort.on('message', (message) => {
+        console.log('---------', threadId, message);
         // parentPort.postMessage(message);
     });
 }
